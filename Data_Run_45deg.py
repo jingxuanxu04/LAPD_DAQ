78a566db4340bb1cc9bed519a29d26ec9b21d06c
Jia Han <jhan@physics.ucla.edu>
2025-03-20 16:29:47 -0700

    Latest changes from first datarun test with 45deg probe

--- Data_Run_45deg.py
+++ Data_Run_45deg.py
@@ -31,21 +31,21 @@ logging.basicConfig(filename='motor.log', level=logging.WARNING,
 '''
 User: Set experiment name and path
 '''
-exp_name = '01-test'  # experiment name
-date = datetime.date.today()
+exp_name = '03_P22_P34_0'  # experiment name
+cur_dt = datetime.datetime.now()
 path = f"C:\\data"
-save_path = f"{path}\\{exp_name}_{date}.hdf5"
+save_path = f"{path}\\{exp_name}_{cur_dt.year}-{cur_dt.month}-{cur_dt.day}-{cur_dt.hour}:{cur_dt.minute}.hdf5"
 
 #-------------------------------------------------------------------------------------------------------------
 '''
 User: Set probe position array
 '''
 # Set up position array (unit: cm)
-xstart = {'P16': 0, 'P22': -40, 'P29': -40, 'P34': -40, 'P42': 0}
-xstop  = {'P16': 0, 'P22': -40, 'P29': -40, 'P34': -40, 'P42': 0}
-nx = 1       # number of positions
+xstart = {'P16': 0, 'P22': -30, 'P29': -40, 'P34': -30, 'P42': 0}
+xstop  = {'P16': 0, 'P22': 16, 'P29': -40, 'P34': 16, 'P42': 0}
+nx = 47       # number of positions
 
-nshot = 1    # number of shots at each position
+nshot = 8    # number of shots at each position
 
 #-------------------------------------------------------------------------------------------------------------
 def get_experiment_description():
@@ -56,39 +56,55 @@ def get_experiment_description():
     
     Experiment: {exp_name}
     Date: {date}
-    Operator: Jia Han
+    Operator: Yhoshua W.
     
     Probe Movement:
     - 45deg probe located at P16, P22, P29, P34, P42
     - Probe moves on single radial line    
     - Number of shots at each position: {nshot}
     - Number of positions: {nx}
-    
-    Setup:
-    - Plasma condition
-        - Heater 2200 A
-        - Puff Helium backside pressure 48 Psi
-        - Puff voltage 95V for 23ms West+East
-        - Hydrogen 200 SCCM MFC is set to "300", or 60 SCCM
-        - Discharge 25 ms; bank charging 92 V; current 4.5 kA
-        - Pulsing 1/4.25 Hz; plasma breakdown ~12 ms
-        - Pressure ~0.14 mTorr
-        - Interferometer density ~1.5e13@P20 ~0.8e13@P29 (assume 40cm)
-    - Magnetic field
-        - Straight 1.2 kG
-        - Black (South) 672 A
-        - Yellow 3120 A
-        - Purple 1092 A
-        - Black (North) 0 A
-    - Antenna (ZZ-#3 six wire mesh paddles wt 1/8inch gap)
-        - At P30 WEST x:-19 y:-2=>4 z: -6=>8
+     
+
+    Purpose: TP 45deg probe drives.  100ks, 50ms window on scope
+
+    LAPD B field:
+    ========
+    Black magnets at south: 1125 A (PS12, 13) 2000 G
+    Yellow magnets: 1000 G
+    Purple magnets:  1000 G 
+
+    South LaB6 source:
+    ============
+    He plasma , discharge PS voltage/current:93V/4460A 
+    2/3 Hz rep rate 
+    Heater: 2.106 kA
+    Fill Pres: 1.3e-4 torr
+    Gas Puff (96.5 V on East and West, 19.4 ms) Helium
+    40 PSI on tank
+
+    Timing:
+    =====
+    Discharge Period: 20 ms
+    Breakdown time: 5 ms
+    DAQ trigger: 0 ms
+    RF turn on: 14ms
+    Scope trigger: -5 ms
+
+    ANTENA:
+    =========
+    RF ON: Driving antenna with black box. capacitors in parallel (0.15x2=0.3 micro F) and capacitor in series (2 micro F)
+    Using steve's antenna (P26-P28).
+    Antenna turn on: 14 ms
+    PS Voltage: 430 V
+    Driver current: 
+    500 cycles (close to 2.5 ms)
 
     - Probe
         - 4-tip Langmuir with Tunsten wire 1mm long
         - Triple probe boxes are used (see https://drive.google.com/drive/folders/1-9VcMYBBSbKsqKTpIVFkImKsi8OC1s4K?usp=drive_link)
         - P16 -- Not active
-        - P22 -- Isat (R knob #4)
-        - P29 -- Isat (R know #5)
+        - P22 -- Isat (R knob #5) 
+        - P29 -- Not active
         - P34 -- Isat (R knob #5)
         - P42 -- Not active
 
@@ -111,17 +127,17 @@ motor_ips = {
 def get_channel_description(tr):
     """Channel description"""
     descriptions = {
-        'Scope_C1': 'Isat',
-        'Scope_C2': 'Isat',
-        'Scope_C3': 'Isat',
-        'Scope_C4': 'N/A'
+        'Scope_C1': 'Isat_P22',
+        'Scope_C2': 'Te_P22',
+        'Scope_C3': 'Isat_P34',
+        'Scope_C4': 'Te_P34'
     }
     return descriptions.get(tr, f'Channel {tr} - No description available')
 
 def get_scope_description(scope_name):
     """Return description for each scope"""
     descriptions = {
-        'FastScope': '''LeCroy '''
+        'Scope': '''LeCroy '''
     }
     return descriptions.get(scope_name, f'Scope {scope_name} - No description available')
 
